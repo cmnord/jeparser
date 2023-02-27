ea17d8ba0980ae028697cad22223ce7ad3445aa4
Claire Nord <cnord@alum.mit.edu>
2023-02-26 21:33:43 -0800

    source: lay out basics of content script and popup script
    1. When the popup script loads, it sends a message to the content script to
       parse the page.
    2. The content script parses the page and responds with JSON of the game.
    3. The popup script shows that game JSON in the popup window and makes a link
       to download it.

--- source/popup/options.ts
+++ source/popup/options.ts
@@ -1,32 +1,66 @@
-const rangeInputs = [
-	...document.querySelectorAll('input[type="range"][name^="color"]'),
-] as HTMLInputElement[];
-const numberInputs = [
-	...document.querySelectorAll('input[type="number"][name^="color"]'),
-] as HTMLInputElement[];
-const output = document.querySelector(".color-output");
+import { Game } from "source/content";
 
-console.log("options.ts running");
+interface Response {
+	game: Game | null;
+	error?: string;
+}
 
-function updateColor() {
-	if (output) {
-		const outputDiv = output as HTMLDivElement;
-		outputDiv.style.backgroundColor = `rgb(${rangeInputs[0].value}, ${rangeInputs[1].value}, ${rangeInputs[2].value})`;
+/** requestGame sends a message to the content script when the popup opens to get
+ * the content of the page. */
+async function requestGame() {
+	const [tab] = await browser.tabs.query({ active: true, currentWindow: true });
+	const tabId = tab.id;
+	if (!tabId) {
+		throw new Error("could not find active tab ID");
+	}
+	const response: Response = await browser.tabs.sendMessage(tabId, {
+		message: "parse",
+	});
+
+	if (response.error) {
+		reportExecuteScriptError(response.error);
+	}
+	const game = response.game;
+	if (!game) {
+		return reportExecuteScriptError("could not parse game");
+	}
+	showGame(game);
+}
+
+/** There was an error executing the script.  Display the popup's error message,
+ * and hide the normal UI. */
+function reportExecuteScriptError(error: string) {
+	document.querySelector("#popup-content")?.classList.add("hidden");
+	document.querySelector("#error-content")?.classList.remove("hidden");
+	console.error(`Failed to execute content script: ${error}`);
+}
+
+/** showGame renders the game JSON in the extension popup and presents a button
+ * to download it. */
+function showGame(game: Game) {
+	const stringifiedGame = JSON.stringify(game, null, "\t");
+	const gameDiv = document.querySelector("#game");
+	if (gameDiv) {
+		gameDiv.classList.remove("hidden");
+		gameDiv.textContent = stringifiedGame;
+	}
+	const downloadLink = document.querySelector("#download-link");
+	if (downloadLink) {
+		downloadLink.setAttribute(
+			"href",
+			"data:text/plain;charset=utf-8," + encodeURIComponent(stringifiedGame)
+		);
+		const fileName = game.title.replace(/[^a-z0-9]/gi, "_").toLowerCase();
+		downloadLink.setAttribute("download", fileName + ".jep.json");
+	}
+	const downloadButton = document.querySelector("#download-button");
+	if (downloadButton) {
+		downloadButton.attributes.removeNamedItem("disabled");
 	}
 }
 
-function updateInputField(event: Event) {
-	const target = event.currentTarget as HTMLInputElement;
-	if (target) {
-		numberInputs[rangeInputs.indexOf(target)].value = target.value;
-	}
-}
-
-for (const input of rangeInputs) {
-	input.addEventListener("input", updateColor);
-	input.addEventListener("input", updateInputField);
-}
-
-window.addEventListener("load", updateColor);
+(async () => {
+	await requestGame();
+})();
 
 export {};
